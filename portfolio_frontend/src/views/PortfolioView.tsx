793296e03e0448cf9e36e74a3075359bf08df386
njoflores <normanjf13@gmail.com>
2026-03-28 16:21:48 +0800

    Add files via upload

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x9721c64bab151a2a7c6bd38e7d96c52ed1fe828617e24f05e32b9e03016485cc";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
